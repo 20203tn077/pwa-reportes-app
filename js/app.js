f02505677e32fd12e7f38716af547d26bb495be0
Rebecca C. <20203tn077@utez.edu.mx>
2023-10-17 18:35:13 -0600

    fix sw location

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 console.log('APP JS')
 const url = window.location.href
 const APIth = 'http://localhost:3000/api'
-let swLocation = 'reportes/sw.js'
+let swLocation = '/sw.js'
 
 if (navigator.serviceWorker) {
   if (url.includes('localhost') || url.includes('127.0.0.1')) swLocation = '/sw.js'
